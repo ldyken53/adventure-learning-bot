82a652e10a86df36d8a8f4694bb3771e37619add
Cameronwood611 <cwood611@uab.edu>
2021-04-30 22:23:57 -0500

    new learn command

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 # STL
 import os
+import random
 
 # PDM
 import discord
@@ -7,13 +8,24 @@ from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
 
 bot = commands.Bot(command_prefix="!")  # , intents=discord.Intents.all()
-slash = SlashCommand(bot)
+slash = SlashCommand(bot, sync_commands=True)
 
 
-@slash.slash(name="test")
-async def _test(ctx: SlashContext) -> None:
-    embed = discord.Embed(title="embed test")
-    await ctx.send(content="test", embeds=[embed])
+@bot.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(bot))
+
+
+@slash.slash(name="test", description="Simple test command", guild_ids=[837844953790808074])
+async def test(ctx: SlashContext) -> None:
+    # embed = discord.Embed(title="embed test")
+    await ctx.send(content="test")
+
+
+
+@slash.slash(name="learn", guild_ids=[837844953790808074])
+async def learn(ctx: SlashContext) -> None:
+    await ctx.send(content="What would you like to learn?")
 
 
 def start() -> None:
