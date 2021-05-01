8d3beef652430ab3e7d8696d4e6da161726a5cc1
Elijah Sink <theminorprofit@gmail.com>
2021-05-01 11:09:15 -0500

    Add more fields to adventure table

--- adventures_db.sql
+++ adventures_db.sql
@@ -1,4 +1,7 @@
 CREATE TABLE IF NOT EXISTS adventures (
+    id uuid PRIMARY KEY,
+    name text NOT NULL,
+    description TEXT NOT NULL DEFAULT '',
     creator text NOT NULL,
     created_on timestamp NOT NULL DEFAULT now(),
     paths jsonb NOT NULL
