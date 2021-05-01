cd157049f7a2888b8ce9d1529930f932de78eb24
Jan Foksinski <jkf@uab.edu>
2021-05-01 00:17:55 -0500

    Add webapp code

---
+++ adventure-workshop/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Welcome to Learn-A-Bot's Adventure Workshop!
+        </p>
+        <button className='bg-blue-900 p-2 rounded'>Begin Creating Your New Adventure</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
