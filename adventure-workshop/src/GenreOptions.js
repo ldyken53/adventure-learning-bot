1736e52f9337cc936ae8f1a6061eca020f59ab51
Jan Foksinski <jkf@uab.edu>
2021-05-01 13:43:34 -0500

    Update with title and desc

--- adventure-workshop/src/GenreOptions.js
+++ adventure-workshop/src/GenreOptions.js
@@ -1,7 +1,7 @@
 /* This example requires Tailwind CSS v2.0+ */
 import { Fragment, Component } from "react";
 import { Listbox, Transition } from "@headlessui/react";
-import { CheckIcon, SelectorIcon } from "@heroicons/react/solid";
+import { SelectorIcon } from "@heroicons/react/solid";
 
 const genres = [
   {
@@ -28,23 +28,13 @@ const genres = [
 
 
 class GenreOptions extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        selected: ''
-    };
-    this.change=this.change.bind(this);
-  }
-
   classNames(...classes) {
     return classes.filter(Boolean).join(" ");
   }
-  change = (e) => {
-    this.setState({selected: e.name});
-  };
   render() {
+    const {genre, update} = this.props;
     return (
-      <Listbox value={this.state.selected} onChange={this.change}>
+      <Listbox value={genre} onChange={update}>
         {({ open }) => (
           <>
             <Listbox.Label className="block font-medium text-3xl text-white-700">
@@ -53,7 +43,7 @@ class GenreOptions extends Component {
             <div className="mt-1 relative">
               <Listbox.Button className="relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                 <span className="flex items-center">
-                  <span className="ml-3 block truncate text-black text-2xl">{this.state.selected}</span>
+                  <span className="ml-3 block truncate text-black text-2xl">{genre}</span>
                 </span>
                 <span className="ml-3 absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
                   <SelectorIcon
@@ -74,16 +64,16 @@ class GenreOptions extends Component {
                   static
                   className="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-56 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm"
                 >
-                  {genres.map((genre) => (
+                  {genres.map((_genre) => (
                     <Listbox.Option
-                      key={genre.id}
+                      key={_genre.id}
                       className={({ active }) =>
                         this.classNames(
                           active ? "text-white bg-indigo-600" : "text-gray-900",
                           "cursor-default select-none relative py-2 pl-3 pr-9"
                         )
                       }
-                      value={genre}
+                      value={_genre}
                     >
                       {({ selected, active }) => (
                         <>
@@ -94,7 +84,7 @@ class GenreOptions extends Component {
                                 "ml-3 block truncate text-2xl"
                               )}
                             >
-                              {genre.name}
+                              {_genre.name}
                             </span>
                           </div>
                         </>
---
+++ adventure-workshop/src/Intro.jsx
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Intro extends Component {
+  constructor(props) {
+    super(props);
+    this.displayForm = this.displayForm.bind(this);
+    this.displayDesc = this.displayDesc.bind(this);
+  }
+
+  displayForm() {
+    const { genre, name, updateTitle } = this.props;
+    return genre !== "" ? (
+      <>
+        <div className="p-3">
+          <label
+            htmlFor="name"
+            className="block p-2 text-3xl font-medium text-white"
+          >
+            What's the catchy name for your adventure?:
+          </label>
+          <div className="mt-1 relative rounded-md shadow-sm">
+            <input
+              type="text"
+              name="name"
+              id="name"
+              value={name}
+              onChange={updateTitle}
+              className="text-black text-3xl focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 border-gray-300 rounded-md"
+              placeholder={"The Greatest" + (" " + genre) + " Adventure"}
+            />
+          </div>
+        </div>
+        {this.displayDesc()}
+      </>
+    ) : (
+      <div className="p-3">
+        <h1 className="p-3">Missing Genre!</h1>
+        <Link className="bg-blue-900 p-2 rounded" id="start" to={"/start"}>
+          Click here to choose a genre
+        </Link>
+      </div>
+    );
+  }
+  displayDesc() {
+    const { genre, name, description, updateDesc } = this.props;
+    return name !== "" ? (
+      <div className="p-3">
+        <label
+          htmlFor="desc"
+          className="block p-2 text-3xl font-medium text-white"
+        >
+          What's a good description of your adventure?:
+        </label>
+        <div className="mt-1 relative rounded-md shadow-sm">
+          <input
+            type="text"
+            name="desc"
+            id="desc"
+            value={description}
+            onChange={updateDesc}
+            className="text-black text-xl focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 border-gray-300 rounded-md"
+            placeholder={
+              "A guided tour into the field of" + (" " + genre) + " ..."
+            }
+          />
+        </div>
+      </div>
+    ) : null;
+  }
+
+  render() {
+    const { genre } = this.props;
+    return <div>{this.displayForm()}</div>;
+  }
+}
+
+export { Intro };
